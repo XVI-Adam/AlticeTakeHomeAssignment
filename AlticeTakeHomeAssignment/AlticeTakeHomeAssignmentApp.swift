a5e7a5dde6bbb34bcce1401cb7161b2472daa82c
XVI-Adam <adammartinez.martinez2@gmail.com>
2026-01-14 01:56:08 -0500

    Initial Commit

---
+++ AlticeTakeHomeAssignment/AlticeTakeHomeAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlticeTakeHomeAssignmentApp.swift
+//  AlticeTakeHomeAssignment
+//
+//  Created by Adam Martinez on 1/14/26.
+//
+
+import SwiftUI
+
+@main
+struct AlticeTakeHomeAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
